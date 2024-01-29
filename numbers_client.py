ea4838030ea165d6d789a367a9ce0936c0fa0b76
LiorDaSelior <liorbelenkov@mail.tau.ac.il>
2024-01-29 18:14:33 +0200

    added socket error handling in client

--- numbers_client.py
+++ numbers_client.py
@@ -23,73 +23,78 @@ elif  len(sys.argv) == 3:
 else:
     raise AttributeError("Client setup failed - Arguments are invalid.")
 
-with sk.socket(sk.AF_INET, sk.SOCK_STREAM) as cilent_socket:
-    
-    cilent_socket.connect((host, port))
-    
-    # recv welcome msg
-    size_in_bytes = cilent_socket.recv(4)
-    size = int.from_bytes(size_in_bytes, "big")
-    current_msg = ns.IncomingSocketMessage(size)
-    while not current_msg.is_complete():
-        current_msg.add_data(cilent_socket.recv(DATA_BANDWIDTH))
-    print(current_msg.data.decode(encoding = "utf-8")) 
-    
-    while not logged_in_flag:
+try:
+    with sk.socket(sk.AF_INET, sk.SOCK_STREAM) as cilent_socket:
         
-        # login attempt
-        first_line_input = input() 
-        second_line_input = input()
-        current_msg = ns.OutgoingSocketMessage(f"{first_line_input}\n{second_line_input}")
-        cilent_socket.send(current_msg.size.to_bytes(4, 'big')) # send login credentials data size
-        while not current_msg.is_complete(): # sending login cred credentials data
-            cilent_socket.send(current_msg.get_data(DATA_BANDWIDTH))
-            
-        # recv server response to login
-        size_in_bytes = cilent_socket.recv(4) 
+        cilent_socket.connect((host, port))
+        
+        # recv welcome msg
+        size_in_bytes = cilent_socket.recv(4)
         size = int.from_bytes(size_in_bytes, "big")
         current_msg = ns.IncomingSocketMessage(size)
         while not current_msg.is_complete():
             current_msg.add_data(cilent_socket.recv(DATA_BANDWIDTH))
-        res = current_msg.data.decode(encoding = "utf-8")
-        print(res)
+        print(current_msg.data.decode(encoding = "utf-8")) 
         
-        # analyze response
-        if res == "Login Failed.":
-            pass
-        elif res == "Disconnected from server.":
-            break
-        else:
-            logged_in_flag = True
-        
-    if logged_in_flag:
-        while True:
+        while not logged_in_flag:
             
-            # enter command and check validity. If invalid -> send 'quit' command, which disconnects client from server
-            cmd = input()
-            try:
-                ns.check_cmd_argument_amount(cmd)
-                ns.execute(cmd)
-            except:
-                print("Error - Command arguments are invalid.") # per instruction, print error msg
-                cmd = "quit"
-            
-            current_msg = ns.OutgoingSocketMessage(cmd)
-            cilent_socket.send(current_msg.size.to_bytes(4, 'big')) # send command data size
-            while not current_msg.is_complete(): # sending command data
+            # login attempt
+            first_line_input = input() 
+            second_line_input = input()
+            current_msg = ns.OutgoingSocketMessage(f"{first_line_input}\n{second_line_input}")
+            cilent_socket.send(current_msg.size.to_bytes(4, 'big')) # send login credentials data size
+            while not current_msg.is_complete(): # sending login cred credentials data
                 cilent_socket.send(current_msg.get_data(DATA_BANDWIDTH))
-            
-            # recv server response to command
+                
+            # recv server response to login
             size_in_bytes = cilent_socket.recv(4) 
             size = int.from_bytes(size_in_bytes, "big")
             current_msg = ns.IncomingSocketMessage(size)
             while not current_msg.is_complete():
                 current_msg.add_data(cilent_socket.recv(DATA_BANDWIDTH))
+            res = current_msg.data.decode(encoding = "utf-8")
+            print(res)
             
             # analyze response
-            res = current_msg.data.decode(encoding = "utf-8") 
-            print(res)
-            if res == "Disconnected from server.":
+            if res == "Login Failed.":
+                pass
+            elif res == "Disconnected from server.":
                 break
+            else:
+                logged_in_flag = True
             
-    print("Program finished.")
+        if logged_in_flag:
+            while True:
+                
+                # enter command and check validity. If invalid -> send 'quit' command, which disconnects client from server
+                cmd = input()
+                try:
+                    ns.check_cmd_argument_amount(cmd)
+                    ns.execute(cmd)
+                except:
+                    print("Error - Command arguments are invalid.") # per instruction, print error msg
+                    cmd = "quit"
+                
+                current_msg = ns.OutgoingSocketMessage(cmd)
+                cilent_socket.send(current_msg.size.to_bytes(4, 'big')) # send command data size
+                while not current_msg.is_complete(): # sending command data
+                    cilent_socket.send(current_msg.get_data(DATA_BANDWIDTH))
+                
+                # recv server response to command
+                size_in_bytes = cilent_socket.recv(4) 
+                size = int.from_bytes(size_in_bytes, "big")
+                current_msg = ns.IncomingSocketMessage(size)
+                while not current_msg.is_complete():
+                    current_msg.add_data(cilent_socket.recv(DATA_BANDWIDTH))
+                
+                # analyze response
+                res = current_msg.data.decode(encoding = "utf-8") 
+                print(res)
+                if res == "Disconnected from server.":
+                    break
+                
+        print("Program finished.")
+except OSError as exception:
+    print(exception.strerror)
+    print("Socket error encountered. Program finished.")
+    exit()
